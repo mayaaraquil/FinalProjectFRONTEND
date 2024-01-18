58dbb68cddf591f61d195ed5ac38838a0adec5b6
Sammy Erickson <0uts1d3th3b0x355@gmail.com>
2024-01-17 20:19:01 -0500

    final pushes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,19 @@ import { SpotifyloginComponent } from "./components/spotifylogin/spotifylogin.co
 import { NgModule } from '@angular/core';
 import { SeachSongsComponent } from './components/seach-songs/seach-songs.component';
 import { LibraryComponent } from './components/library/library.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { CreateBlogFormComponent } from './components/create-blog-form/create-blog-form.component';
 
 
 const routes: Routes = [
+    {path: '', component:HomepageComponent},
     {path: 'profile', component:ProfileComponent},
     {path: 'spotifylogin', component: SpotifyloginComponent},
     {path: 'search', component:SeachSongsComponent},
-    {path: 'library', component:LibraryComponent}
+    {path: 'library', component:LibraryComponent},
+    {path: 'blogs', component:BlogComponent},
+    {path: 'createBlog', component:CreateBlogFormComponent}
 ];
 
 @NgModule({
